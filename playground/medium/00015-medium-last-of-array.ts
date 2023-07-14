5dd49eccf6d8f0f7fc096f14ee696ba17f70ed53
kuromitsu0104 <kuromitsu0104@gmail.com>
2023-07-14 21:46:15 +0900

    feat: 回答を追加する

--- playground/medium/00015-medium-last-of-array.ts
+++ playground/medium/00015-medium-last-of-array.ts
@@ -24,12 +24,13 @@
 
 /* _____________ ここにコードを記入 _____________ */
 
-type Last<T extends any[]> = any
+type Last<T extends any[]> = [any, ...T][T['length']]
 
 /* _____________ テストケース _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
+  Expect<Equal<Last<[]>, any>>,
   Expect<Equal<Last<[2]>, 2>>,
   Expect<Equal<Last<[3, 2, 1]>, 1>>,
   Expect<Equal<Last<[() => 123, { a: string }]>, { a: string }>>,
